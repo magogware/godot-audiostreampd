543f84a5cdab921de22d6d19e61739a7f6383ee8
Alex Barilaro <alex@barilaro.me>
2022-03-16 13:50:28 +1100

    Add icon

--- addons/audiostreampd/plugin.gd
+++ addons/audiostreampd/plugin.gd
@@ -2,7 +2,7 @@ tool
 extends EditorPlugin
 
 func _enter_tree():
-	add_custom_type("PdStreamHelper", "Node", preload("PdStreamHelper.gd"), preload("icon.png"))
+	add_custom_type("PdStreamHelper", "Node", preload("PdStreamHelper.gd"), preload("icon.svg"))
 
 func _exit_tree():
 	remove_custom_type("PdStreamHelper")
